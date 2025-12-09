40bd6335d61f9b63b675f8ee0077c6d78f57c3a3
Mangoeu <martin.jakubec2@educanet.cz>
2025-12-09 11:06:43 +0100

    zakladni objekt php

---
+++ objekt.php
@@ -0,0 +1,43 @@
+<?php 
+
+class cars {
+    public $name;
+    public $spotreba;
+    public $nadrz;
+    
+
+      function input_spotreba($name, $spotreba, $nadrz) {
+        $this->name = $name;  
+        $this->spotreba = $spotreba;
+        $this->nadrz = $nadrz;
+  }
+
+  function get_name() {
+    return $this->name;
+    }
+    function get_spotreba() {
+    return $this->spotreba;
+    }
+
+    function get_nadrz() {
+    return $this->nadrz;
+    }
+
+}
+
+$Auta = new cars();
+$Auta->input_spotreba("BMW",5,55);
+
+
+echo "Spotřeba " . $Auta-> get_name(). " je ".$Auta ->get_spotreba(). " itrů na 100 km.";
+
+echo "<br>";
+
+echo "Nádrž je ve stavu: " . $Auta ->get_nadrz() . " litrů.";
+
+
+
+
+
+
+?>
